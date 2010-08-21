1752bbcc9ba8546bd8c3b7dc264235c0d138ebdb
Dan <git@dandart.co.uk>
2010-08-21 16:53:59 +0100

    added media sharing

---
+++ apps/media/sharing/app.php
@@ -0,0 +1,16 @@
+<div id="sharing" class="app_small">
+<script>
+function share_media(file) {
+alert(file);
+}
+</script>
+
+<h1>Share Media</h1>
+<form name='sharing' onsubmit='return false'>
+<input type='hidden' name='type' />
+<p>Type: <span id='sharing_type'></span></p>
+<p>Title: <input type='text' name='title' /></p>
+<p>Description: <textarea name='desc'></textarea></p>
+<p><input type="submit" value='Share!' />
+
+</div>
